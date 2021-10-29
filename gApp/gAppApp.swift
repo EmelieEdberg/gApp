aa609a9e4a5b900566e319b4801ebf6e3c9e845f
Emelie Edberg <familjen.emelie@gmail.com>
2021-10-29 19:38:52 +0200

    Initial Commit

---
+++ gApp/gAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gAppApp.swift
+//  gApp
+//
+//  Created by Emelie Edberg on 2021-10-29.
+//
+
+import SwiftUI
+
+@main
+struct gAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
